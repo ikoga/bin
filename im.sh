f1fd7de9e3f43a1d94fe12b45eb53bd5d8136ccf
Isamu Koga <koga@iij.ad.jp>
2021-03-21 16:16:17 +0900

    どの環境にどの変数を使うかコメントを追加

--- im.sh
+++ im.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# KDE3/TDE のときは xim、Plasma のときは fcitx か ibus
 echo -e "QT_IM_MODULE\t${QT_IM_MODULE}"
+
+# 使われているのかわからない (たぶん使っていない)
 echo -e "QT4_IM_MODULE\t${QT4_IM_MODULE}"
+
+# GNOME のときは fcitx か ibus
 echo -e "GTK_IM_MODULE\t${GTK_IM_MODULE}"
+
+# xterm は @im=fcitx か @im=ibus
 echo -e "XMODIFIERS\t${XMODIFIERS}"
-
-
