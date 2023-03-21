53f8dbfefa2318ba0b2f7ff135050c836272f0ef
Isamu Koga <koga@iij.ad.jp>
2023-03-21 23:44:04 +0900

    openSUSE Leap 15.4, VAIO ProPK 用に書き換え

--- caption_stat.sh
+++ caption_stat.sh
@@ -3,15 +3,16 @@ LANG=C
 BATTERY_DIR="BAT1"
 WARN=30
 
-calc_bm () {
-   remain=`cat /proc/acpi/battery/${BATTERY_DIR}/state | grep 'remaining' | sed -e 's/^.*: *//' -e 's/ mWh$//'`
-   full=`cat /proc/acpi/battery/${BATTERY_DIR}/info | grep 'last full' | sed -e 's/^.*: *//' -e 's/ mWh$//'`
-#   full=`cat /proc/acpi/battery/${BATTERY_DIR}/info | grep 'design capacity:' | awk '{ print $3 }'`
-   percent=$(( remain * 100 / full ))
-   echo $percent
-}
-
-BATSTAT=`calc_bm`
+#calc_bm () {
+#   remain=`cat /proc/acpi/battery/${BATTERY_DIR}/state | grep 'remaining' | sed -e 's/^.*: *//' -e 's/ mWh$//'`
+#   full=`cat /proc/acpi/battery/${BATTERY_DIR}/info | grep 'last full' | sed -e 's/^.*: *//' -e 's/ mWh$//'`
+##   full=`cat /proc/acpi/battery/${BATTERY_DIR}/info | grep 'design capacity:' | awk '{ print $3 }'`
+#   percent=$(( remain * 100 / full ))
+#   echo $percent
+#}
+# BATSTAT=`calc_bm`
+#
+BATSTAT=`fgrep "POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT1/uevent | awk -F '=' '{ print $2 }'`
 if [ $BATSTAT -lt $WARN ]; then
    echo -n "Warning! ${BATSTAT}%"
 else
@@ -19,26 +20,20 @@ else
 fi
 
 
-STATE=`fgrep "charging state" /proc/acpi/battery/${BATTERY_DIR}/state | awk '{ print $NF }'`
-if [ "$STATE" = "charged" ]; then
+STATE=`cat /sys/class/power_supply/BAT1/status`
+if [ "$STATE" = "Charged" ]; then
    echo -n " 充電済"
 
-elif [ "$STATE" = "charging" ]; then
+elif [ "$STATE" = "Charging" ]; then
    echo -n " 充電中"
 
-elif [ "$STATE" = "discharging" ]; then
+elif [ "$STATE" = "Discharging" ]; then
    echo -n " 放電中"
 
 else
    echo -n "[?]"
 
 fi
-
-# tenki.sh
-# echo -n " "
-# cat /var/tmp/tenki.txt | tr -d '\n'
-echo -n " "
-cat /var/tmp/cal.txt
 
 
 ## Wi-Fi Frequency
